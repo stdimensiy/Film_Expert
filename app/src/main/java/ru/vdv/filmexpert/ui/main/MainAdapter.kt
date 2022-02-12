4cdb6766dfd32daaef630942e991e4a85f91e3b9
Dmitriy <stdimensiy@yandex.ru>
2022-02-12 23:48:51 +0300

    Изм 0.8 Разработка главного экрана и связь с данными - реализован цветной прогрессбар, в зависомости от значения рейтинга сам прогресс изменяет цвет

--- app/src/main/java/ru/vdv/filmexpert/ui/main/MainAdapter.kt
+++ app/src/main/java/ru/vdv/filmexpert/ui/main/MainAdapter.kt
@@ -1,15 +1,18 @@
 package ru.vdv.filmexpert.ui.main
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import ru.vdv.filmexpert.R
 import ru.vdv.filmexpert.domain.MovieTmdb
 import ru.vdv.filmexpert.model.api.TmdbApiConstants
 import ru.vdv.myapp.myreadersdiary.glide.GlideImageLoader
 import ru.vdv.myapp.myreadersdiary.glide.ImageLoader
 
-class MainAdapter: RecyclerView.Adapter<MainViewHolder>() {
+class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
     private val imageLoader: ImageLoader<ImageView> = GlideImageLoader()
     var items: List<MovieTmdb> = listOf()
 
@@ -17,13 +20,42 @@ class MainAdapter: RecyclerView.Adapter<MainViewHolder>() {
         return MainViewHolder(LayoutInflater.from(parent.context), parent)
     }
 
+    @SuppressLint("ResourceType")
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val item = items[position]
+        val roundedRatingValue = (item.voteAverage * 10).toInt()
         holder.name.text = item.title
         holder.data.text = item.releaseDate
-        holder.rating.text = (item.voteAverage*10).toInt().toString()
-        holder.ratingBar.setProgressCompat((item.voteAverage*10).toInt(), true)
-        imageLoader.loadMovieCover(String.format(TmdbApiConstants.POSTER_URL, item.posterPath), holder.posterImage)
+        val viewContext = holder.itemView.context
+        val rBar = holder.ratingBar
+        holder.rating.text = roundedRatingValue.toString()
+        rBar.setProgressCompat(roundedRatingValue, true)
+
+        when (roundedRatingValue) {
+            in 0..5 -> {
+                rBar.setIndicatorColor(Color.RED)
+                rBar.setProgressCompat(5, true)
+            }
+            in 6..10 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_010)))
+            in 11..15 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_015)))
+            in 16..20 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_020)))
+            in 21..25 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_025)))
+            in 26..30 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_030)))
+            in 31..35 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_035)))
+            in 40..60 -> rBar.setIndicatorColor(Color.YELLOW)
+            in 61..65 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_040)))
+            in 66..70 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_045)))
+            in 71..75 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_050)))
+            in 76..80 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_060)))
+            in 81..85 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_070)))
+            in 86..90 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_080)))
+            in 91..95 -> rBar.setIndicatorColor(Color.parseColor(viewContext.getString(R.color.red_to_green_090)))
+            in 96..10000 -> rBar.setIndicatorColor(Color.GREEN)
+        }
+            imageLoader.loadMovieCover(
+            String.format(TmdbApiConstants.POSTER_URL, item.posterPath),
+            holder.posterImage
+        )
     }
 
     override fun getItemCount(): Int {
